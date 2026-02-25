874b54af25b2b6117b8bb47b5ff7796f9f7f225b
Aswinnath TE <aswinthalaa123123@gmail.com>
2026-02-25 17:44:34 +0530

    Update DSU.py

--- DSU.py
+++ DSU.py
@@ -1,19 +1,26 @@
 class DSU:
 	def __init__(self, sizes: int) -> None:
-		self.parents = [i for i in range(sizes+1)]
-		self.sizes = [1 for _ in range(sizes+1)]
+		self.parents = [i for i in range(sizes + 1)]
+		self.sizes = [1 for _ in range(sizes + 1)]
 
-	def find(self, x: int) -> int:return x if x==self.parents[x] else self.find(self.parents[x])
+	def find(self, x: int) -> int:
+		if x == self.parents[x]:
+			return x
+		return self.find(self.parents[x])
 
 	def merge(self, x: int, y: int) -> bool:
 		x_root = self.find(x)
 		y_root = self.find(y)
+
 		if x_root == y_root:
 			return False
 
 		if self.sizes[x_root] < self.sizes[y_root]:
 			x_root, y_root = y_root, x_root
+
 		self.parents[y_root] = x_root
 		self.sizes[x_root] += self.sizes[y_root]
 		return True
-	def same(self, x: int, y: int):return self.find(x) == self.find(y)
+
+	def same(self, x: int, y: int) -> bool:
+		return self.find(x) == self.find(y)
